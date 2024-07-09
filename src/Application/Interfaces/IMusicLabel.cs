a5bb2147b47dd66239e7b483532aa2b4c9672929
BC0LL1N5 <brennencollins44@gmail.com>
2024-07-09 18:59:46 -0500

    changed string to int. in app & interface

---
+++ src/Application/Interfaces/IMusicLabel.cs
@@ -0,0 +1,22 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces;
+public interface IMusicLabel
+{
+    Task<string> CreateMusicLabel(MusicLabel musicLabel);
+
+    Task<string> DeleteMusicLabel(int musicLabelId);
+
+    Task<List<MusicLabel>> GetAllMusicLabels();
+
+    Task<string> UpdateMusicLabel(MusicLabel musicLabel);
+
+
+
+
+}
